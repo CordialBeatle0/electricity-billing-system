d802eb9d654375f125be18bf6a4e1d393f4f4741
Amir Osama <amirosama21@gmail.com>
2024-04-28 21:34:00 +0300

    assar mshh 3abitaa <3

--- src/Cash.java
+++ src/Cash.java
@@ -6,52 +6,50 @@ import java.sql.Statement;
 import java.time.LocalDateTime;
 
 public class Cash implements Payment {
-	public Cash() {
-	}
+    public Cash() {
+    }
 
-	@Override
-	public void makePayment(Customer customer, float amount) {
-		Request req = new Request(customer.getID(), customer.getName(),"Collect Cash Payment", customer.getAddress(), LocalDateTime.now());
-		//add request to DB
-		req.addRequestToDB();
-		req.requestHomeService();
-	}
-	
-	public void confirmCashPayment(Technician technician, Request request, float payedAmount) {
-		int custID = request.getCustID();
-		DatabaseSingleton db = DatabaseSingleton.getInstance();
-		Connection conn = db.getConnection();
-		try {
-			Statement stmt = conn.createStatement();
-			String sql = "SELECT * FROM customer WHERE id = " + custID;
-			ResultSet rs = stmt.executeQuery(sql);
-			if (rs.next()) {
-				float outstandingFees = rs.getFloat("outstandingFees");
-				if (outstandingFees == 0.0) {
-					JOptionPane.showMessageDialog(null, "Customer with ID "
-							+ custID + " has no outstanding fees.");
-				} else if (payedAmount > outstandingFees) {
-					JOptionPane.showMessageDialog(null, " Amount is grater than outstaanding");
-					return;
-				} else {
-					// Update the customer's outstanding fees to 0
-					String update = "UPDATE customer SET outstandingFees = "
-							+ (outstandingFees - payedAmount) + " WHERE id = " + custID;
-					stmt.executeUpdate(update);
-					String delete = "DELETE FROM request WHERE id = " + request.getID();
-					stmt.executeUpdate(delete);
-					String isTimeToPayFalse = "UPDATE customer SET isTimeToPay = FALSE WHERE id = " + custID;
-					stmt.executeUpdate(isTimeToPayFalse);
-					JOptionPane.showMessageDialog(null, "Customer with ID "
-							+ custID + " has paid "
-							+ payedAmount + " remaining fees: " + (outstandingFees - payedAmount) + "$. ");
-				}
-			} else {
-				System.out.println("Customer with ID " + custID + " not found.");
-				JOptionPane.showMessageDialog(null, "Customer with ID " + custID + " not found.");
-			}
-		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "Error editing or retrieving customer from database");
-		}
-	}
+    @Override
+    public void makePayment(Customer customer, float amount) {
+        Request req = new Request(customer.getID(), customer.getName(), "Collect Cash Payment", customer.getAddress(), LocalDateTime.now());
+        req.requestHomeService();
+    }
+
+    public void confirmCashPayment(Technician technician, Request request, float payedAmount) {
+        int custID = request.getCustID();
+        DatabaseSingleton db = DatabaseSingleton.getInstance();
+        Connection conn = db.getConnection();
+        try {
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM customer WHERE id = " + custID;
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                float outstandingFees = rs.getFloat("outstandingFees");
+                if (outstandingFees == 0.0) {
+                    JOptionPane.showMessageDialog(null, "Customer with ID "
+                            + custID + " has no outstanding fees.");
+                } else if (payedAmount > outstandingFees) {
+                    JOptionPane.showMessageDialog(null, " Amount is grater than outstaanding");
+                    return;
+                } else {
+                    // Update the customer's outstanding fees to 0
+                    String update = "UPDATE customer SET outstandingFees = "
+                            + (outstandingFees - payedAmount) + " WHERE id = " + custID;
+                    stmt.executeUpdate(update);
+                    String delete = "DELETE FROM request WHERE id = " + request.getID();
+                    stmt.executeUpdate(delete);
+                    String isTimeToPayFalse = "UPDATE customer SET isTimeToPay = FALSE WHERE id = " + custID;
+                    stmt.executeUpdate(isTimeToPayFalse);
+                    JOptionPane.showMessageDialog(null, "Customer with ID "
+                            + custID + " has paid "
+                            + payedAmount + " remaining fees: " + (outstandingFees - payedAmount) + "$. ");
+                }
+            } else {
+                System.out.println("Customer with ID " + custID + " not found.");
+                JOptionPane.showMessageDialog(null, "Customer with ID " + custID + " not found.");
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error editing or retrieving customer from database");
+        }
+    }
 }
